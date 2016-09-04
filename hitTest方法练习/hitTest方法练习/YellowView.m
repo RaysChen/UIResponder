47800b289d16532610eef223a72d467dad5c5f33
RaysChen <543114160@qq.com>
2016-09-04 22:22:27 +0800

    UIResponder
    一点文档

--- hitTest方法练习/hitTest方法练习/YellowView.m
+++ hitTest方法练习/hitTest方法练习/YellowView.m
@@ -6,6 +6,15 @@
 //  Copyright © 2016年 chenxi. All rights reserved.
 //
 
+/**
+ ** 分析思路：如果一个点，同时在黄色view和按钮上，由按钮处理事件，怎么做?
+ * 分析事件传递: 当黄色要处理事件，首先事件得传递到他身上
+ * 重写hitTest方法：事件传递到某个控件，调用什么方法?hitTest
+ * 返回nil什么意思？如果直接返回nil，意味着黄色的view，没有找到最合适的view,他的父控件，就会遍历下一个控件，也就是按钮，询问按钮是不是最合适的view.
+ *
+ *
+ */
+
 #import "YellowView.h"
 @interface YellowView()
 
