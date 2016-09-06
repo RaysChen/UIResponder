6d3fe84c302367651b3f565c88fbb03cc8712dad
RaysChen <543114160@qq.com>
2016-09-06 19:17:23 +0800

    UIResponder
    实现一个简单的抽屉效果

---
+++ 实现一个简单的抽屉效果/实现一个简单的抽屉效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  实现一个简单的抽屉效果
+//
+//  Created by 陈曦 on 16/9/6.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
